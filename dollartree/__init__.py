74b68f52a0b775ac6867e2fedb0ca798568a5d30
Chaz Larson <chaz@chazlarson.com>
2021-09-22 15:58:03 -0500

    dollartree module

---
+++ dollartree/__init__.py
@@ -0,0 +1,166 @@
+
+
+# online individual bags colors; min qty 5
+# Black, white, red, blue, yellow, green
+# 0,    15,   4,  1,   14,    2
+
+# online by-the-case colors; min qty 45
+# Blue, Gray, Green, Red, Yellow
+# 1,     71,   2,     4,   14
+
+# photo shows brown
+# Brown
+# 6
+
+# "Roofs" 39277-37799 UPC $1/8 pc [.125/each]
+# plate 6x8 3036 4 brown
+# plate 2x8 3034 4 brown
+
+# Scenery 39277-37817 UPC $1/20 pc [.05/each]
+# plate 4x4 3031 1 green
+# plate 2x2 3022 2 green
+# Slope 45 2 x 1 No: 3040 3 brown
+# plate 1x2 3023 1 brown
+# cheese 1x1 54200 1 brown
+# plate 1x1 round 6141 4 green
+# Arch 1 x 5 x 4 - Continuous Bow Item No: 2339 3 brown
+# Plant Flower Small, 4 on Sprue Item No: 3742c01 1 red
+# 2423 Plant Leaves 4 x 3 4 green
+# non-lego 1x1 bush 2 green
+
+# "windows and doors" 39277-37806 UPC $1/28 pc [.035/each]
+# LEGO PART 90195 Window 1 x 2 x 2 Castle 6 black
+# plate 1x2 3023 2 red
+# plate 1x2 with rail 32028 2 red
+# plate 1x1 vert clip 15712 4 red
+# Plate Special 1 x 2 with Handle on End [Closed Ends] 60478 4 red
+# Tile 2 x 4 with Groove 87079 2 red
+# Window 1 x 2 x 3 Pane 3854 8 WHITE
+# non-lego 1x4x3 window frame
+
+# brick	2x6 2456
+# brick	2x4 3001
+# brick	2x3 3002
+# brick	2x2 3003
+# brick	1x4 3010
+# brick	1x2 3004
+# brick	1x1 3005
+# plate	2x6 3795
+# plate	2x4 3020
+# plate	2x3 3021
+# plate	2x2 3022
+# plate	1x6 3666
+# plate	1x4 3710
+# plate	1x2 3023
+# plate	1x1 3024
+
+# Make-It Blocks Building Blocks, 100-pc. box 328541 $1/100 pc (.01/each); min qty 24
+# brick	1x1 3005 25
+# brick	1x1 3005 25
+# brick	1x1 3005 25
+# brick	1x1 3005 25
+# brick	1x1 3005 25
+# brick	1x2 3004 20
+# brick	1x4 3010 20
+# brick	2x2 3003 20
+# brick	2x3 3002 20
+# brick	2x4 3001 20
+# in grey - 71, green - 2, blue - 1, brown - 6
+
+# brick code, array of colors
+dtbricks_328541 = {
+    '3001': {'colors': [1, 2, 6, 71], 'qty': 25}, 
+    '3002': {'colors': [1, 2, 6, 71], 'qty': 25},
+    '3003': {'colors': [1, 2, 6, 71], 'qty': 25},
+    '3004': {'colors': [1, 2, 6, 71], 'qty': 25},
+    '3005': {'colors': [1, 2, 6, 71], 'qty': 25},
+    '3010': {'colors': [1, 2, 6, 71], 'qty': 25}
+}
+
+# online by-the-case colors; min qty 45
+# Blue, Gray, Green, Red, Yellow
+# 1,     71,   2,     4,   14
+dtbricks_online_case = {
+    '3001': {'colors': [1, 2, 4, 14, 71], 'qty': 2},
+    '3002': {'colors': [1, 2, 4, 14, 71], 'qty': 3},
+    '3003': {'colors': [1, 2, 4, 14, 71], 'qty': 5},
+    '3010': {'colors': [1, 2, 4, 14, 71], 'qty': 3},
+    '3004': {'colors': [1, 2, 4, 14, 71], 'qty': 3},
+    '3020': {'colors': [1, 2, 4, 14, 71], 'qty': 6},
+    '3021': {'colors': [1, 2, 4, 14, 71], 'qty': 3},
+    '3022': {'colors': [1, 2, 4, 14, 71], 'qty': 9},
+    '3710': {'colors': [1, 2, 4, 14, 71], 'qty': 8},
+    '3023': {'colors': [1, 2, 4, 14, 71], 'qty': 6}
+    }
+
+# white 313871
+# black 313865
+# yellow 313875
+# red 313868
+# green 313872
+# blue 313877
+
+dtbricks_online_bag_small = {
+    '3001': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 2},
+    '3002': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 3},
+    '3003': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 5},
+    '3010': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 3},
+    '3004': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 3},
+    '3020': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 6},
+    '3021': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 3},
+    '3022': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 9},
+    '3710': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 8},
+    '3023': {'colors': [0, 15, 4, 1, 14, 2], 'qty': 6}
+    }
+
+# 32x32 baseplate 3811
+dtbricks_online_each = {
+    '3811': {'colors': [1, 2, 6, 71], 'qty': 1}
+    }
+
+# TODO: confirm that these are still available anywhere
+# "in-store big"
+# | 2456 | 2x6 brick |  6 | 0 |
+# | 3001 | 2x4 brick | 10 | 2 |
+# | 3003 | 2x2 brick |  6 | 5 |
+# | 3010 | 1x4 brick |  6 | 3 |
+# | 3020 | 2x4 plate |  5 | 6 |
+# | 3022 | 2x2 plate |  5 | 9 |
+# | 3666 | 1x6 plate |  5 | 0 |
+# | 3710 | 1x4 plate |  5 | 8 |
+
+# "in-store small"
+# | 3001 | 2x4 brick | 10 | 2 |
+# | 3002 | 2x3 brick |  0 | 3 |
+# | 3003 | 2x2 brick |  6 | 5 |
+# | 3010 | 1x4 brick |  6 | 3 |
+# | 3004 | 1x2 brick |  0 | 3 |
+# | 3020 | 2x4 plate |  5 | 6 |
+# | 3021 | 2x3 plate |  0 | 3 |
+# | 3022 | 2x2 plate |  5 | 9 |
+# | 3710 | 1x4 plate |  5 | 8 |
+# | 3023 | 1x2 plate |  0 | 6 |
+
+def isDTBrick(partNum, partColor):
+    dtHasIt = False
+
+# This is offensive to the eyes
+    try:
+        thePart = dtbricks_328541[partNum]
+        dtHasIt = thePart['colors'].__contains__(partColor)
+    except:
+        try:
+            thePart = dtbricks_online_bag_small[partNum]
+            dtHasIt = thePart['colors'].__contains__(partColor)
+        except:
+            try:
+                thePart = dtbricks_online_case[partNum]
+                dtHasIt = thePart['colors'].__contains__(partColor)
+            except:
+                try:
+                    thePart = dtbricks_online_each[partNum]
+                    dtHasIt = thePart['colors'].__contains__(partColor)
+                except:
+                    dtHasIt = False
+    
+    return dtHasIt
